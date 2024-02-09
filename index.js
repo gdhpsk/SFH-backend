599a6f5a85d787814c9563dbd971632b25f1d9bc
gdhpsk <70782191+gdhpsk@users.noreply.github.com>
2024-02-08 18:56:07 -0800

    ws support for geode

--- index.js
+++ index.js
@@ -4,6 +4,17 @@ const cors = require("cors")
 const cookieParser = require("cookie-parser")
 const dotenv = require("dotenv")
 const app = express()
+const {WebSocketServer} = require("ws")
+const songs = require("./schemas/songs")
+const http_server = require("http").createServer(app)
+
+let server = new WebSocketServer({server: http_server})
+
+server.on("connection", socket => {
+        let uuid = crypto.randomUUID()
+        socket.send(uuid)
+        socket.uuid = uuid
+}) 
 
 if(!process.env.MONGODB_URI) {
     dotenv.config()
@@ -23,8 +34,24 @@ app.use(express.urlencoded({extended: true}))
 app.use(cookieParser())
 
 app.set("view engine", "ejs")
+
+app.get("/socket/:id", async (req, res) => {
+    if(!req.query.hash) return res.status(400).json({error: "400 BAD REQUEST", message: "Provide a hash to send to the ws client."})
+    let sock = Array.from(server.clients).find(e => e.uuid == req.params.id)
+    if(!sock) return res.status(400).json({error: "400 BAD REQUEST", message: "Could not find socket."})
+    let urlHash;
+    try {
+        urlHash = await songs.findById(req.query.hash)
+        if(!urlHash) throw new Error()
+    } catch(_) {
+        return res.status(400).json({error: "400 BAD REQUEST", message: "Could not find hash."})
+    }
+    sock.send(urlHash.urlHash)
+    sock.close()
+    return res.sendStatus(204)
+})
  
 app.use("/", require("./api"))
 
 console.log("Listening on port http://localhost:3000")
-app.listen(process.env.PORT || 3000)
+http_server.listen(process.env.PORT || 3000)
