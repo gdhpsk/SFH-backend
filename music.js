478b544fb93a8d48fa658ce9d3685cc5747700b5
gdhpsk <70782191+gdhpsk@users.noreply.github.com>
2024-10-20 15:42:14 -0700

    added in the _02 version of music lib links for 2.206

--- music.js
+++ music.js
@@ -9,4 +9,12 @@ app.get("/musiclibrary_version.txt", (req, res) => {
     return res.redirect("/v2/songs?format=version")
 })
 
+app.get("/musiclibrary_02.dat", (req, res) => {
+    return res.redirect("/v2/songs?format=library")
+})
+
+app.get("/musiclibrary_version_02.txt", (req, res) => {
+    return res.redirect("/v2/songs?format=version")
+})
+
 module.exports = app
