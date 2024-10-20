85dd24542c6aa96cd8a93bb87684d5d5fcc670d4
gdhpsk <70782191+gdhpsk@users.noreply.github.com>
2024-10-20 14:54:11 -0700

    using undici pool to make concurrent reqs to the bucket
    limited at 256

--- api.js
+++ api.js
@@ -6,6 +6,12 @@ const { ObjectId } = require("bson")
 const adminsSchema = require("./schemas/admins")
 const app = express.Router()
 const zlib = require("zlib")
+const {Pool} = require("undici")
+
+const pool = new Pool('https://storage.hpsk.me', {
+    connections: 256, 
+    pipelining: 1
+  });
 
 // public
 
@@ -56,43 +62,56 @@ app.get("/v2/songs", async (req, res) => {
 		case 'gd':
 			let array = []
 			for(const song of songs) {
-					(async () => {
-						let songData = await fetch(`https://storage.hpsk.me/api/bucket/file/${song.urlHash}?onlyMetadata=true`)
-					let metadata = await songData.json()
-					array.push(`1~|~${song.songID}~|~2~|~${song.songName}~|~4~|~SongFileHub~|~5~|~${Math.round(metadata.size / 10000)/100}~|~10~|~${song.downloadUrl}`)
-					})()
+                (async () => {
+                    let songData = await pool.request({
+                        path: `/api/bucket/file/${song.urlHash}?onlyMetadata=true`,
+                        method: "GET"
+                    })
+                    let metadata = await songData.body.json()
+                    array.push(`1~|~${song.songID}~|~2~|~${song.songName}~|~4~|~SongFileHub~|~5~|~${Math.round(metadata.size / 10000)/100}~|~10~|~${song.downloadUrl}`)
+                })()
 			}
-			let alr = setInterval(() => {
-				if(songs.length == array.length) {
-					clearInterval(alr)
-					res.json(array.join(":"))
-				}
-			}, 0)
-			return;
-			break;
+           await new Promise((resolve, reject) => {
+            let alr = setInterval(() => {
+                if(songs.length == array.length) {
+                    clearInterval(alr)
+                    return resolve("")
+                }
+            }, 10)
+           })
+            return res.json(array.join(":"))
 		case 'library':
 			let songsString = '';
 			let tagsDictionary = {rated: 0, unrated: 1, mashup: 2, challenge: 3, remix: 4, loop: 5};
-			let tagsArray = ['800000,Rated', '800001,Unrated', '800002,Mashup', '800003,Challenge', '800004,Remix', '800005,Menu loop'];
 			let tag = '';
+            let count = 0;
 			for(const song of songs) {
 				(async () => {
-					let songData = await fetch(`https://storage.hpsk.me/api/bucket/file/${song.urlHash}?onlyMetadata=true`)
-					let metadata = await songData.json()
+					let songData = await pool.request({
+                        path: `/api/bucket/file/${song.urlHash}?onlyMetadata=true`,
+                        method: "GET"
+                    })
+					let metadata = await songData.body.json()
 					tag = '80000' + tagsDictionary[song.state];
 					songsString += `${song.songID},${song.songName},${song.songID},800006,${metadata.size},0,${tag},0, , , ,0;`
+                    count++;
 				})()
 			}
+            await new Promise((resolve, reject) => {
+                let alr = setInterval(() => {
+                    if(songs.length == count) {
+                        clearInterval(alr)
+                        return resolve("")
+                    }
+                }, 10)
+               })
 			let libraryVersion = songs.length;
 			let library = libraryVersion + '|800006,SongFileHub;|' + songsString + '|800000,Rated;800001,Unrated;800002,Mashup;800003,Challenge;800004,Remix;800005,Menu loop;'
 			var deflated = zlib.deflateSync(library).toString("base64url");
 			return res.send(deflated);
-			break;
 		default:
 			return res.json(songs);
-			break;
 	}
-return res.status(400).json({error: "400 BAD REQUEST", message: "Please input a valid format!"})
 })
 
 app.get("/songs", async (req, res) => {
@@ -142,18 +161,21 @@ app.get("/songs", async (req, res) => {
     if(req.query.format == "gd") {
         let array = []
         for(const song of songs) {
-                (async () => {
-                    let songData = await fetch(song.downloadUrl + "?onlyMetadata=true")
-                let metadata = await songData.json()
+            (async () => {
+                let songData = await pool.request({
+                    path: `/api/bucket/file/${song.urlHash}?onlyMetadata=true`,
+                    method: "GET"
+                })
+                let metadata = await songData.body.json()
                 array.push(`1~|~${song.songID}~|~2~|~${song.songName}~|~4~|~SongFileHub~|~5~|~${Math.round(metadata.size / 10000)/100}~|~10~|~${song.downloadUrl}`)
-                })()
+            })()
         }
         let alr = setInterval(() => {
             if(songs.length == array.length) {
                 clearInterval(alr)
                 res.json(array.join(":"))
             }
-        }, 0)
+        }, 10)
         return
     }
     if(req.query.format == "sfh") {
