b5f986b92d95585c028c3a617420f5ae6aa5c0a1
gdhpsk <70782191+gdhpsk@users.noreply.github.com>
2025-01-23 17:06:51 -0800

    Update api.js
    Need to be safe lol

--- api.js
+++ api.js
@@ -9,7 +9,7 @@ const zlib = require("zlib")
 const {Pool} = require("undici")
 
 const pool = new Pool('https://storage.hpsk.me', {
-    connections: 256, 
+    connections: 64, 
     pipelining: 1
   });
 
